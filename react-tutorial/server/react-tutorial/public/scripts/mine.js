b47763698002311283275802f306e956e3f86d2a
chauhansk <Kamalpreet.chauhan@advancedcomputersoftware.com>
2015-10-15 18:12:06 +0100

    Finish tutorial 15

--- react-tutorial/server/react-tutorial/public/scripts/mine.js
+++ react-tutorial/server/react-tutorial/public/scripts/mine.js
@@ -1,8 +1,27 @@
  var CommentBox = React.createClass({
     getInitialState: function(){
       return{
-        data: [];
+        data: []
       }
+    },
+    
+    loadCommentsFromServer: function(){
+      $.ajax({
+        url: this.props.url,
+        dataType: 'json',
+        cache:false,
+        success: function(data){
+          this.setState({data: data});
+        }.bind(this),
+        error: function(xhr, status, err){
+          console.error(this.props.url, status, err.toString());
+        }.bind(this)
+      });
+    },
+
+    componentDidMount: function(){
+      this.loadCommentsFromServer();
+      setInterval(this.loadCommentsFromServer, this.props.pollInterval);
     },
 
     render: function(){
@@ -63,6 +82,6 @@ var Comment = React.createClass({
 });
 
 ReactDOM.render(
-  <CommentBox url="/api/comments" />,
+  <CommentBox url="/api/comments" pollInterval="2000" />,
   document.getElementById('content')
 );
